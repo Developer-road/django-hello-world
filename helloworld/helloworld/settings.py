a36f23b6a81bbf1343ded570bb83b2f9f0008f38
Daniel Diaz <danieldiazgonza52@gmail.com>
2021-03-07 21:40:16 -0500

    Added Stuff

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'myapp',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- helloworld/myapp/views.py
+++ helloworld/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+def helloworldview(request):
+
+    template = "index.html"
+
+    people = ["daniel", "nicolas", "tom", "henry"]
+
+    context = {"people": people}    
+
+    return render(request, template, context)
